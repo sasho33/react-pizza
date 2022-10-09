be3108e66b8aa87ecaf5d635bfdd248757577a0e
sasho33 <sasho3333333@gmail.com>
2022-10-09 18:34:55 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+
+          some of my text
         </p>
         <a
           className="App-link"
